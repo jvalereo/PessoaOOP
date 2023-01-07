a73455e966818aa9e5cb93cc4da598086a5ee2f2
jvalereo <jvalereo2016@gmail.com>
2023-01-07 16:31:58 -0300

    Projeto Pessoa OOP

---
+++ Aula10/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10
+{
+    class Pessoa
+    {
+        //declarando os atributos, campos,variaveis da class pessoa
+
+        public string nome;
+        public string sobrenome;
+        public string sexo;
+        public int idade;
+        public float peso;
+        public string pais;
+
+
+        //declarando os métodos / função  da classe do pessoa
+
+        public void Acorda()
+        {
+            Console.WriteLine("Acorda de manhã");
+        }
+
+        public void Anda()
+        {
+            Console.WriteLine("Caminha pela ruas");
+        }
+
+        public void Come()
+        {
+            Console.WriteLine("Come alimentos");
+        }
+
+        public void Dormi()
+        {
+            Console.WriteLine("Dormi a noite");
+        }
+
+        //fim da classe pessoa
+    }
+}
---
+++ Aula10/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Aula10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Treinamento - Aula 10, Programação Orientação ao Objeto
+            //Inserindo dados, tipo string , int, float
+            //Jonas Valereo - Técnico em Informática 
+
+            //declarando o objeto pessoa da class pessoa
+
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("Digite o seu nome: ");
+            pessoa.nome = Console.ReadLine();
+            Console.WriteLine("Digite o seu sobrenome: ");
+            pessoa.sobrenome = Console.ReadLine();
+            Console.WriteLine("Digite o seu sexo: ");
+            pessoa.sexo = Console.ReadLine();
+            Console.WriteLine("Digite o sua idade: ");
+            pessoa.idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o seu peso: ");
+            pessoa.peso = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o seu pais: ");
+            pessoa.pais = Console.ReadLine();
+
+            //imprimir saida de dados no console, e método WhriteLine, concatenação
+            Console.WriteLine(" ");
+            Console.WriteLine("Nome: " + pessoa.nome);
+            Console.WriteLine("Sobrenome: " + pessoa.sobrenome);
+            Console.WriteLine("Sexo: " + pessoa.sexo);
+            Console.WriteLine("Idade: " + pessoa.idade);
+            Console.WriteLine("Peso: " + pessoa.peso.ToString("F2", CultureInfo.InvariantCulture) + " Kg");
+            Console.WriteLine("Pais: " + pessoa.pais);
+            Console.WriteLine(" ");
+
+            //chamando o método da class pessoa e imprimindo
+            Console.WriteLine("----------------------");
+            pessoa.Acorda();
+            pessoa.Anda();
+            pessoa.Come();
+            pessoa.Dormi();
+
+            //saida do aplicativo console, método Readkey, Retorna o valor da tecla pressionada
+
+            Console.ReadKey();
+
+            //fim do programa
+
+        }
+    }
+}
